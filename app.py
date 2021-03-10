9a0b32c8a80b98965f2ed2a897f2745a9e9cf481
m-zz <marcusyzhu@gmail.com>
2021-03-10 12:06:00 -0800

    added templates and add user route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, flash, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,37 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route("/")
+def home():
+    return redirect('/users')
+
+@app.route("/users")
+def userlist():
+
+    a = User(first_name = 'John', last_name = 'Smith')
+    db.session.add(a)
+    db.session.commit()
+
+    users = User.query.all()
+    return render_template('list.html', users = users)
+
+@app.route("/users/new")
+def new_user():
+
+    return render_template('user_form.html')
+
+@app.route("/users/new/add", methods = ['POST'])
+def add_user():
+
+    # f_name, l_name, img_url = request.form -- can we destructure?
+    f_name = request.form['f_name']
+    l_name = request.form['l_name']
+    img_url = request.form['img_url']
+
+
+    user = User(first_name = f_name, last_name = l_name, image_url = img_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
--- models.py
+++ models.py
@@ -1,1 +1,21 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True, auto_increment = True)
+    first_name = db.Column(db.String(30), nullable = False)
+    last_name = db.Column(db.String(30), nullable = False)
+    image_url = db.Column(db.String(), default = "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg")
